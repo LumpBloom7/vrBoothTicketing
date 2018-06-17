e37b129e693beedb99368ddb8b3282f67b8176da
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-06-17 16:53:58 +0800

    Factored time into booth management

--- Reservation.cpp
+++ Reservation.cpp
@@ -1,77 +1,98 @@
 #include "booth.hpp"
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+
 namespace booth {
   int guestCount = 0;
   class Reservation {
   public:
     Reservation();
-    Reservation( int startTime, std::string contactDetails, int computerNumber )
-        : startTime( startTime ), contactDetails( contactDetails ), computerNumber( computerNumber ) {
+    Reservation( std::chrono::system_clock::time_point startTime, std::string name, std::string contactDetails,
+                 int computerNumber )
+        : startTime( startTime ), name( name ), contactDetails( contactDetails ), computerNumber( computerNumber ) {
       guestCount++;
       guestNumber = guestCount;
-      endTime = startTime + 15;
+      endTime = startTime + std::chrono::seconds( 900 );
       // Generate random verification code here.
     }
-    int startTime;
+    std::string name;
+    std::chrono::system_clock::time_point startTime;
     std::string contactDetails{""};
-    int verificationCode;
-    int endTime = startTime + 15;
+    int verificationCode = 0;
+    std::chrono::system_clock::time_point endTime = startTime + std::chrono::seconds( 900 );
     int computerNumber;
     int guestNumber;
   };
   std::vector<Reservation> guests{};
-  std::vector<std::vector<Reservation>> computers{};
+  std::vector<std::vector<Reservation>> computers = std::vector<std::vector<Reservation>>( 4 );
 
-  void addReservation() {
+  void addReservation() { /*
+     if ( guestCount == 0 ) {
+       timeZero = timer.count<std::chrono::seconds>();
+       startTime = std::chrono::system_clock::now();
+     }*/
     cligCore::console::clear();
-    std::string name;
+    std::string name = "";
     std::cout << "Please input the customer's english or romanised name." << std::endl;
-    std::cin >> name;
-    int computerNumber;
+    while ( name == "" ) { std::getline( std::cin, name ); }
+    std::cout << "Please enter one or more way to contact you(Phone, Email; Seperated by commas)." << std::endl;
+    std::string contactDetails;
+    std::cin >> contactDetails;
+    std::clog << "1";
+    std::chrono::system_clock::time_point currentTime =
+        std::chrono::ceil<std::chrono::minutes>( std::chrono::system_clock::now() );
+    std::clog << "2";
     bool init = false;
-    int smallestComputer;
-    int computerSize;
-    for ( int i = 3; i >= 0; i-- ) {
-      if ( !init ) {
-        smallestComputer = i;
-        computerSize = computers[ i ].size();
+    int smallestNumber;
+    for ( int i = 0; i < 4; i++ ) {
+      if ( computers[ i ].size() == 0 ) {
+        smallestNumber = i;
+        break;
       } else {
-        if ( computers[ i ].size() <= computerSize ) {
-          smallestComputer = i;
-          computerSize = computers[ i ].size();
+        if ( i == 0 )
+          smallestNumber = i;
+        else {
+          if ( computers[ i ][ computers[ i ].size() - 1 ].endTime <
+               computers[ smallestNumber ][ computers[ smallestNumber ].size() - 1 ].endTime )
+            smallestNumber = i;
         }
       }
     }
-    int startTime = computers[ smallestComputer ][ computerSize - 1 ].endTime;
+    std::chrono::system_clock::time_point startTime;
+    if ( computers[ smallestNumber ].size() == 0 )
+      startTime = currentTime;
+    else if ( computers[ smallestNumber ][ computers[ smallestNumber ].size() - 1 ].endTime < currentTime )
+      startTime = currentTime;
+    else
+      startTime = computers[ smallestNumber ][ computers[ smallestNumber ].size() - 1 ].endTime;
 
-    std::cout << "Successfully found a suitable time for your session." << std::endl
-              << "Please enter one or more way to contact you(Phone, Email; Seperated by commas)." << std::endl;
+    Reservation guest = Reservation( startTime, name, contactDetails, smallestNumber );
 
-    std::string contactDetails;
-    std::cin >> contactDetails;
-
-    Reservation guest = Reservation( startTime, contactDetails, smallestComputer );
-
-    computers[ smallestComputer ].emplace_back( guest );
+    computers[ smallestNumber ].emplace_back( guest );
     guests.emplace_back( guest );
 
     cligCore::console::clear();
-
+    std::time_t start = std::chrono::system_clock::to_time_t( guest.startTime );
+    std::time_t end = std::chrono::system_clock::to_time_t( guest.endTime );
+    std::cout << "Successfully found a suitable time for your session." << std::endl;
     std::cout << "Please write this down..." << std::endl
-              << termcolor::cyan << "User number: " << guest.guestNumber << std::endl
+              << termcolor::cyan << "User name: " << guest.name << std::endl
+              << "User number: " << guest.guestNumber << std::endl
               << "Computer number: " << guest.computerNumber << std::endl
               << "Verification code: " << guest.verificationCode << std::endl
               << std::endl
-              << "Start time: " << guest.startTime << std::endl
-              << "End time: " << guest.endTime << std::endl
+              << "Start time: " << std::put_time( std::localtime( &start ), "%H:%M" ) << std::endl
+              << "End time: " << std::put_time( std::localtime( &end ), "%H:%M" ) << std::endl
               << std::endl
-              << "It is recommended that you arrive at least 5 minutes before your start time. Failure to arrive on "
-                 "time may result in your play session being shorter"
-              << std::endl;
+              << "It is recommended that you arrive at least 5 minutes before your start time." << std::endl
+              << "Failure to arrive on time may result in your play session being shorter" << std::endl;
+    std::cin.ignore();
+    std::cin.ignore();
   }
 
   void verifyCode() {
     cligCore::console::clear();
-    std::cin.ignore();
     int userNumber;
     std::cout << "Please enter your user number: " << termcolor::green << std::flush;
     std::cin >> userNumber;
@@ -83,5 +104,27 @@ namespace booth {
     int verificationCode;
     std::cout << termcolor::reset << "Please enter your verification code: " << termcolor::green << std::flush;
     std::cin >> verificationCode;
+
+    cligCore::console::clear();
+    if ( userNumber > 0 && guests.size() >= userNumber )
+      if ( guests[ userNumber - 1 ].verificationCode == verificationCode &&
+           computerNumber == guests[ userNumber - 1 ].computerNumber ) {
+        std::cout << "Verification Successful." << std::endl;
+        if ( guests[ userNumber - 1 ].startTime - std::chrono::system_clock::now() > std::chrono::minutes( 10 ) ) {
+          std::cout << "But it is a bit early don't you think?" << std::endl
+                    << "Please try again later, preferably about 5 minutes before your start time." << std::endl;
+        } else
+          std::cout << "A staff member will now guide you to your computer" << std::endl;
+      }
+
+      else
+        std::cout << "Verification failed." << std::endl
+                  << "One or more of the inputted values are incorrect" << std::endl;
+    else
+      std::cout << "Verification failed." << std::endl
+                << "One or more of the inputted values are incorrect" << std::endl;
+
+    std::cin.ignore();
+    std::cin.ignore();
   }
 } // namespace booth
--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 #include "cligcore/clig.cpp"
 #include "Reservation.cpp"
+
 int main() {
   while ( true ) {
     std::vector<std::string> menuContent{"1. Add entry", "2. Code Verification", "3. About", "4. Quit"};
     std::cout << "VR Booth Ticketing" << std::endl;
     int result = cligCore::menu::createMenu( "Please select an action", menuContent, false );
     switch ( result ) {
-    case 0: booth::addReservation();
-    case 1: booth::verifyCode();
+    case 0: booth::addReservation(); break;
+    case 1: booth::verifyCode(); break;
     case 2: main(); break;
     case 3: exit( 0 );
     }
