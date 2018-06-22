2850b637bfef04def256dded77bb62aced67e33e
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-06-22 21:03:40 +0800

    Added about screen
    Shows developer and software version

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@
 #include "cligcore/clig.cpp"
 #include "Reservation.cpp"
 
+void about() {
+  cligCore::console::clear();
+  std::string tmp;
+  std::cout << "Booth ticketing system for CHKL 2018 Festival" << std::endl
+            << "Developed by the CHKL Computer Society under the MIT license." << std::endl
+            << std::endl
+            << "Git repository: https://github.com/LumpBloom7/VRBoothTicketing/" << std::endl
+            << "Git Version: " << GIT_VERSION << std::endl
+            << "Git Commit: " << GIT_COMMIT << std::endl
+            << "Git Date: " << GIT_DATE << std::endl
+            << "Build Date: " << BUILD_DATE << std::endl;
+
+  std::getline( std::cin, tmp );
+}
 int main() {
   while ( true ) {
     std::vector<std::string> menuContent{"1. Add entry", "2. Code Verification", "3. About", "4. Quit"};
@@ -14,7 +28,7 @@ int main() {
     switch ( result ) {
     case 0: booth::addReservation(); break;
     case 1: booth::verifyCode(); break;
-    case 2: main(); break;
+    case 2: about(); break;
     case 3: exit( 0 );
     }
   }
